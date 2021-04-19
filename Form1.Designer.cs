68abcd04f16e3f7346c01efdc83453ed5b1345d2
Oleg Petrov <mr.petroll@mail.ru>
2021-04-20 00:17:29 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace рлс
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkKhaki;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(12, 72);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 54);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Установить на карте";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            this.button1.MouseLeave += new System.EventHandler(this.button1_MouseLeave);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Выберите РЛС:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1л117",
+            "35н6",
+            "39н6",
+            "55ж6",
+            "19ж6"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 28);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "ЛА1",
+            "ЛА2",
+            "ЛА3",
+            "ЛА4",
+            "ЛА5"});
+            this.comboBox2.Location = new System.Drawing.Point(12, 158);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(160, 28);
+            this.comboBox2.TabIndex = 8;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 132);
+            this.label2.Margin = new System.Windows.Forms.Padding(3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(127, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Выберите ЛА:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkKhaki;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(12, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(130, 54);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Установить на карте";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = global::рлс.Properties.Resources.Безымянный2;
+            this.pictureBox1.Location = new System.Drawing.Point(178, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(880, 495);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 252);
+            this.label3.Margin = new System.Windows.Forms.Padding(3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.ClientSize = new System.Drawing.Size(1102, 583);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RLS Computing";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace рлс
+{
+    public partial class Form1 : Form
+    {
+        int currentRLS = 0;
+        bool[] installedRLS = new bool[5];
+        bool drawRLS = false;
+
+        bool drawLA = false;
+        int currentLA = 0;
+        bool[] installedLA = new bool[5];
+
+        bool isOnEarth = true;
+        bool isOnAir = true;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            drawRLS = true;
+            drawLA = false;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            label3.Text = (Convert.ToString(e.X) + " " + Convert.ToString(e.Y));
+            heightCheck(e);
+            Graphics g = pictureBox1.CreateGraphics();
+            Image part;
+            if(drawRLS)
+            {
+                if (isOnEarth)
+                {
+                    if (!installedRLS[0] && currentRLS == 1)
+                    {
+                        part = new Bitmap(Properties.Resources.рлс_иконка_1);
+                        g.DrawImage(part, e.X, e.Y);
+                        installedRLS[0] = true;
+                    }
+                    if (!installedRLS[1] && currentRLS == 2)
+                    {
+                        part = new Bitmap(Properties.Resources.рлс_иконка_2);
+                        g.DrawImage(part, e.X, e.Y);
+                        installedRLS[1] = true;
+                    }
+                    if (!installedRLS[2] && currentRLS == 3)
+                    {
+                        part = new Bitmap(Properties.Resources.рлс_иконка_3);
+                        g.DrawImage(part, e.X, e.Y);
+                        installedRLS[2] = true;
+                    }
+                    if (!installedRLS[3] && currentRLS == 4)
+                    {
+                        part = new Bitmap(Properties.Resources.рлс_иконка_4);
+                        g.DrawImage(part, e.X, e.Y);
+                        installedRLS[3] = true;
+                    }
+                    if (!installedRLS[4] && currentRLS == 5)
+                    {
+                        part = new Bitmap(Properties.Resources.рлс_иконка_5);
+                        g.DrawImage(part, e.X, e.Y);
+                        installedRLS[4] = true;
+                    }
+                }
+            }
+            else if(drawLA)
+            {
+                if (isOnAir)
+                {
+                    if (!installedLA[0] && currentLA == 1)
+                    {
+                        part = new Bitmap(Properties.Resources.рлс_иконка_1);
+
+                        g.DrawImage(part, e.X, e.Y);
+                        installedLA[0] = true;
+                    }
+                    if (!installedLA[1] && currentLA == 2)
+                    {
+                        part = new Bitmap(Properties.Resources.рлс_иконка_2);
+                        g.DrawImage(part, e.X, e.Y);
+
+                        installedLA[1] = true;
+                    }
+                    if (!installedLA[2] && currentLA == 3)
+                    {
+                        part = new Bitmap(Properties.Resources.рлс_иконка_3);
+                        g.DrawImage(part, e.X, e.Y);
+                        installedLA[2] = true;
+                    }
+                    if (!installedLA[3] && currentLA == 4)
+                    {
+                        part = new Bitmap(Properties.Resources.рлс_иконка_4);
+                        g.DrawImage(part, e.X, e.Y);
+                        installedLA[3] = true;
+                    }
+                    if (!installedLA[4] && currentLA == 5)
+                    {
+                        part = new Bitmap(Properties.Resources.рлс_иконка_5);
+                        g.DrawImage(part, e.X, e.Y);
+                        installedLA[4] = true;
+                    }
+                }
+            }
+            
+        }
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            label3.Text = (Convert.ToString(e.X) + " " + Convert.ToString(e.Y));
+            Graphics g = pictureBox1.CreateGraphics();
+        }
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            drawRLS = false;
+            drawLA = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            drawLA = true;
+            drawRLS = false;
+        }
+        void heightCheck(MouseEventArgs e)
+        {
+            if (e.Y > pictureBox1.Size.Height*0.53 && e.Y < pictureBox1.Size.Height)
+            {
+                isOnEarth = true;
+            }
+            else isOnEarth = false;
+            if (e.Y > 0 && e.Y <= pictureBox1.Size.Height * 0.53)
+            {
+                isOnAir = true;
+            }
+            else isOnAir = false;
+            if(drawRLS && !isOnEarth)
+            {
+                MessageBox.Show("РЛС нужно ставить на землю.");
+            }
+            if(drawLA && !isOnAir)
+            {
+                MessageBox.Show("ЛА нужно устанавливать на небо.");
+            }
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedRLS = comboBox1.SelectedItem.ToString();
+            if (selectedRLS == "1л117")
+            {
+                currentRLS = 1;
+            }
+            if (selectedRLS == "35н6")
+            {
+                currentRLS = 2;
+            }
+            if (selectedRLS == "39н6")
+            {
+                currentRLS = 3;
+            }
+            if (selectedRLS == "55ж6")
+            {
+                currentRLS = 4;
+            }
+            if (selectedRLS == "19ж6")
+            {
+                currentRLS = 5;
+            }
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedLA = comboBox2.SelectedItem.ToString();
+            if (selectedLA == "ЛА1")
+            {
+                currentLA = 1;
+            }
+            if (selectedLA == "ЛА2")
+            {
+                currentLA = 2;
+            }
+            if (selectedLA == "ЛА3")
+            {
+                currentLA = 3;
+            }
+            if (selectedLA == "ЛА4")
+            {
+                currentLA = 4;
+            }
+            if (selectedLA == "ЛА5")
+            {
+                currentLA = 5;
+            }
+        }
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.FromArgb(141, 136, 63);
+        }
+        private void button1_MouseLeave(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.FromArgb(189, 183, 107);
+        }
+
+        
+    }
+    public class RLS
+    {
+        double LocationX, LocationY;
+        string type;
+
+        double D;//длина?
+        double Power;//излучаемая мощность
+        double G0;//коэфф усил
+        double Lambda;//длина волны
+        double Sensivity;//чувствительность P пр мин
+        double q;//параметр обнаружения
+        double K;//результирующий коэффициент потерь 1.23 1.44 1.58
+        double Potential;//приведенный потенциал
+        double DeltaW;// эффективная полоса пропускания
+        double LambdaRLS;// длина волны рлс
+        double TimeS;//длительность импульсного сигнала на выходе приемника
+        double TetaE;//ширина диграммы направленности антенны (ФАР) в плоскости Е, размерность которой определяет размерность ошибок
+        double Kg;//0.6 ili 0.4
+    }
+    public class SKO
+    {
+        double SigmaD;//ошибка измерения дальности
+        double SigmaV2;//ошибка в измерении скорости
+        double SigmaE;//ошибка в измерении угловых координат
+    }
+    public class LA
+    {
+        double LocationX, LocationY;
+        string type;
+
+        double Surface;//эфф отраж поверхность цели
+    }
+}
+    
